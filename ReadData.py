eaa353b022adc2f3387d9af5e450d7b653765f67
MMT <m.m.talaie@live.com>
2020-02-06 01:01:36 +0330

    create dataset and complet read data

--- ReadData.py
+++ ReadData.py
@@ -1,5 +1,12 @@
 import glob
 
-
-dirs = glob.glob("./tabnak-dataset/*")
-print(dirs)
+textstr = '#text='
+fils = glob.glob("./tabnak-dataset/*")
+with open('dataset/data.txt', 'w') as dataset:
+    for file in fils:
+        with open(file) as fr:
+            for line in fr.readlines():
+                if textstr in line:
+                    dataset.writelines(line.replace(textstr, ''))
+            fr.close()
+    dataset.close()
